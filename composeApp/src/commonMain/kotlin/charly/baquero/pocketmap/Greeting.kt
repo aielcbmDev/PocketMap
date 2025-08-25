2f07c0d493b82406dbf3dcef29721b2d21c8c151
Charly Baquero <aielcbm@gmail.com>
2025-08-25 12:43:23 +0200

    Remove default multiplatform code. Added Swift package resolved to gitignore.

--- composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package charly.baquero.pocketmap
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/Platform.kt
+++
@@ -1,7 +0,0 @@
-package charly.baquero.pocketmap
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
