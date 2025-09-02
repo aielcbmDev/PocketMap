9da454d9063f86f3d852bd7bddf96505ad4c3176
Charly Baquero <aielcbm@gmail.com>
2025-09-02 12:06:15 +0200

    Fixed issue parsing generics due to type erasure.

--- composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/utils/AssetFileProvider.kt
+++ composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/utils/AssetFileProvider.kt
@@ -5,12 +5,12 @@ import pocketmap.composeapp.generated.resources.Res
 
 class AssetFileProvider() {
 
-    private suspend fun readJsonFile(filePath: String): String {
+    internal suspend fun readJsonFile(filePath: String): String {
         val byteArray = Res.readBytes(filePath)
         return byteArray.decodeToString()
     }
 
-    suspend fun <T> getDataList(filePath: String): List<T> {
+    internal suspend inline fun <reified T> getDataList(filePath: String): List<T> {
         val jsonDataList = readJsonFile(filePath)
         return Json.decodeFromString<List<T>>(jsonDataList)
     }
