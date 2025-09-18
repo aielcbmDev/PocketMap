c326744f8a61004fc81f4e4004b8e2479e50dca8
Charly Baquero <aielcbm@gmail.com>
2025-09-18 13:15:37 +0200

    Disable edge to edge.

--- composeApp/src/androidMain/kotlin/charly/baquero/pocketmap/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/charly/baquero/pocketmap/MainActivity.kt
@@ -3,14 +3,12 @@ package charly.baquero.pocketmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
