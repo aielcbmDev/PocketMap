f1e17fcbc733c0bd372ec1134d53ecd278787f23
Charly Baquero <aielcbm@gmail.com>
2025-10-03 20:41:31 +0200

    Remove `hashCode` and `equals` overrides from `GroupModel`.
    The `hashCode` and `equals` methods have been removed from the `GroupModel` data class. This change allows the default data class implementation, which compares all properties, to be used instead of only comparing the `id`.

--- composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/ui/model/GroupModel.kt
+++ composeApp/src/commonMain/kotlin/charly/baquero/pocketmap/ui/model/GroupModel.kt
@@ -6,7 +6,4 @@ import kotlinx.serialization.Serializable
 data class GroupModel(
     val id: Long,
     val name: String,
-) {
-    override fun hashCode() = id.hashCode()
-    override fun equals(other: Any?) = other?.let { id == (it as GroupModel).id } ?: false
-}
+)
