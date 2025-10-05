18a0cae2b0dc598ba77fbc1a9dc7eeba8dacbdb7
Charly Baquero <aielcbm@gmail.com>
2025-10-05 11:05:27 +0200

    Add tests for `DeleteGroupsUseCase`.
    This commit introduces unit tests for the `DeleteGroupsUseCase` to verify its behavior.
    
    The tests cover two scenarios:
    - A success case, ensuring the corresponding repository function is called.
    - A failure case, confirming that exceptions from the repository are correctly propagated.

---
+++ domain/src/commonTest/kotlin/com/charly/domain/usecases/delete/DeleteGroupsUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.charly.domain.usecases.delete
+
+import com.charly.domain.repositories.delete.DeleteGroupsRepository
+import dev.mokkery.answering.returns
+import dev.mokkery.answering.throws
+import dev.mokkery.everySuspend
+import dev.mokkery.mock
+import dev.mokkery.verify.VerifyMode
+import dev.mokkery.verifySuspend
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class DeleteGroupsUseCaseTest {
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `Verify that execute succeeds`() = runTest {
+        // GIVEN
+        val groupIdsSelected = setOf(1L, 2L)
+        val deleteGroupsRepository = mock<DeleteGroupsRepository>() {
+            everySuspend { execute(groupIdsSelected) } returns Unit
+        }
+
+        val deleteGroupsUseCase = DeleteGroupsUseCase(deleteGroupsRepository)
+
+        // WHEN
+        deleteGroupsUseCase.execute(groupIdsSelected)
+        runCurrent()
+
+        // THEN
+        verifySuspend(mode = VerifyMode.Companion.exhaustiveOrder) {
+            deleteGroupsRepository.execute(groupIdsSelected)
+        }
+    }
+
+    @Test
+    fun `Verify that execute fails`() = runTest {
+        // GIVEN
+        val expectedException = Exception("Error")
+        val groupIdsSelected = setOf(1L, 2L)
+        val deleteGroupsRepository = mock<DeleteGroupsRepository>() {
+            everySuspend { execute(groupIdsSelected) } throws expectedException
+        }
+        val deleteGroupsUseCase = DeleteGroupsUseCase(deleteGroupsRepository)
+
+        // WHEN
+        val actualException = assertFailsWith<Exception> {
+            deleteGroupsUseCase.execute(groupIdsSelected)
+            runCurrent()
+        }
+
+        // THEN
+        assertSame(expectedException, actualException)
+        verifySuspend(mode = VerifyMode.Companion.exhaustiveOrder) {
+            deleteGroupsRepository.execute(groupIdsSelected)
+        }
+    }
+}
